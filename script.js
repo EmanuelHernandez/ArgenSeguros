5aad5b1b891e36059b9709b1d016ab7af3645130
Michael <michael69.piro@gmail.com>
2021-08-22 13:41:42 -0300

    add behavior on menu icon, change styles

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Page load')
+
+const menu = document.getElementById('menu-icon')
+
+function menuAction() {
+    const container = document.getElementById('container-links')
+
+    if (container.style.display === 'flex') {
+        container.style.display = 'none'
+    } else {
+        container.style.display = 'flex';
+    }
+}
+
+menu.addEventListener('click', menuAction)
