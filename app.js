38b5d0a5453c557b91e68348941c0773004a0ac6
EmanuelHernandez <hernandezemanuelm@gmail.com>
2021-09-12 14:03:11 -0300

    Nuevas funcionalidades y cambios de estilos

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxdbu9NpxPUFnsA_lvHCP_g2eMhKzIkSiu7QZwOv_YrWdlFFMuA/exec'
+const form = document.forms['submit-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
